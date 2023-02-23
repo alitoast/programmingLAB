8d4d4149fbcf6e282ea02540d87c5666d5870f15
alitoast <115220946+alitoast@users.noreply.github.com>
2023-02-24 00:40:58 +0100

    Update esame.py

--- esame.py
+++ esame.py
@@ -75,4 +75,5 @@ def detect_similar_monthly_variations(time_series, years):
 
     return variation
 
-print(detect_similar_monthly_variations(time_series_file.get_data(), [year,year+1]))
+print(detect_similar_monthly_variations(time_series_file.get_data(), [1956,1957]))
+print(time_series_file.get_data())
